f9a7d4ec3e20ad3b1d84720a47a4a156e5d7e6e1
Raphael Guillemin <raphael.guillemin@umontreal.ca>
2019-07-18 13:06:53 -0400

    commentaires supplémentaires

--- mymalloc.c
+++ mymalloc.c
@@ -1,20 +1,21 @@
 // Vous devez modifier ce fichier pour le TP 2
 // Tout votre code doit être dans ce fichier
-
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "mymalloc.h"
 
 void *mymalloc(size_t size){
-  // À modifier
+  // doit retourner une plage mémoire assez large.
   return malloc(size);
 }
 
 int refinc(void *ptr){
-  // À modifier
+  // incrémenter un compteur interne
   return 0;
 }
 
 void myfree(void *ptr){
-  // À modifier
+  // décrémente le compteur et libère la mémoire si et seulement si ce dernier est à zéro.
   free(ptr);
 }
