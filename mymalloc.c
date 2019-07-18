3bda5a92ceee6b3fcf5d357bd64264a8a2eca8d8
laurabegin <laurabegin1@gmail.com>
2019-07-18 16:47:11 -0400

    linked list implementation

--- mymalloc.c
+++ mymalloc.c
@@ -6,34 +6,56 @@
 #include "mymalloc.h"
 
 void *memory;
+int chunkSize = 4096;
 
 typedef struct block block;
 struct block{
+	int adress;
 	size_t size;
-	void *adress;
 	int count;
 	block *nextBlock;
 };
 
 typedef struct memoryChunk memoryChunk;
 struct memoryChunk{
-	void *adress;
 	block *head;
 	int remainingSize;
+	memoryChunk *nextChunk;
 };
+
+void* block_get (memoryChunk *chunk, int adress) {
+	if (!chunk) return NULL;
+	block *block = chunk->head;
+	while(block->adress + block->size < adress && block->nextBlock != NULL) {
+		block = block->nextBlock;
+	}
+	return &block;
+}
+
+void* chunk_get (void *memory, int byte) {
+	if (!memory) return NULL;
+	memoryChunk *chunk = memory;
+	while(byte > 0 && chunk->nextChunk != NULL) {
+		chunk = chunk->nextChunk;
+		byte = byte - 4096;
+	}
+}
 
 
 void *mymalloc(size_t size){
   // doit retourner une plage mémoire assez large.
   // première étape : définir un bloc de 4*1024 octets avec malloc
-  	if (&memory==NULL){
+  	if (&memory == NULL){
 		memoryChunk *chunk = malloc(4*1024);
-		chunk.remainingSize = 4*1024;
+		block block = {0, size, 1, NULL};
+		chunk->head = &block;
+		chunk->remainingSize = 4*1024 - block.size;
+		memory = &chunk;
+	} else {
+
 	}
 
 
-
-	remainingSize=remainingSize-size;
 	
 }
 
