e653c7f8b5c9ab2473e9afaccfa4edc4970131a2
Raphael Guillemin <raphael.guillemin@umontreal.ca>
2019-07-18 19:50:22 -0400

    malloc à corriger pour compilation mais complet, tous les cas couverts

--- mymalloc.c
+++ mymalloc.c
@@ -7,6 +7,7 @@
 
 void *memory;
 int chunkSize = 4096;
+int numberOfChunks;
 
 typedef struct block block;
 struct block{
@@ -19,10 +20,12 @@ struct block{
 typedef struct memoryChunk memoryChunk;
 struct memoryChunk{
 	block *head;
+	//maxSizeBetweenBlocks contient également les metadonnees des blocks
 	int maxSizeBetweenBlocks;
 	memoryChunk *nextChunk;
 };
 
+//SUREMENT PU VALIDE DEPUIS QU'ON SAIT QU'IL FAUT LES SIZEOF
 void* block_get (memoryChunk *chunk, int adress) {
 	if (!chunk) return NULL;
 	block *block = chunk->head;
@@ -32,6 +35,7 @@ void* block_get (memoryChunk *chunk, int adress) {
 	return &block;
 }
 
+//SUREMENT PU VALIDE DEPUIS QU'ON SAIT QU'IL FAUT LES SIZEOF
 void* chunk_get (void *memory, int byte) {
 	if (!memory) return NULL;
 	memoryChunk *chunk = memory;
@@ -43,37 +47,79 @@ void* chunk_get (void *memory, int byte) {
 }
 
 void *mymalloc(size_t size){
-  // doit retourner une plage mémoire assez large.
-  // première étape : définir un bloc de 4*1024 octets avec malloc
+  	// doit retourner une plage mémoire assez large.
+	//Si aucun chunk, créer premier chunk et premier block
   	if (&memory == NULL){
 		memoryChunk *chunk = malloc(chunkSize);
 		block block = {0, size, 1, NULL};
 		chunk->head = &block;
 		chunk->maxSizeBetweenBlocks = chunkSize - block.size - sizeof(block);
 		memory = &chunk;
-		return &block+sizeof(block);
+		numberOfChunks = 1;
+		return &block + sizeof(block);
 	} else {
+		//Si chunk deja présent, vérifier que le premier contient assez d'espace pour nouveau block
 		memoryChunk *chunk = &memory;
 		//Verification taille maximale entre deux blocs est plus grande que la taille souhaitée + la taille de la métadonnée block
-		if (chunk->maxSizeBetweenBlocks > size + sizeof(block)){
+		if (chunk->maxSizeBetweenBlocks >= size + sizeof(block)){
 			block *currentBlock = chunk->head;
-			//Si le premier bloc du chunck n'est pas suivi ou si espace suffisant entre deux blocs
-			while(currentBlock->nextBlock !=NULL && currentBlock->nextBlock->adress - currentBlock->size + sizeof(currentBlock) < size){
+			//Tant que le premier bloc du chunck n'est pas suivi ou si espace insuffisant entre deux blocs, passer au prochain
+			while(currentBlock->nextBlock != NULL && currentBlock->nextBlock->adress - (currentBlock->size + currentBlock->adress) < size){
 				currentBlock = currentBlock->nextBlock;
 			}
-			block *newBlock = {currentBlock->size + currentBlock->adress + 1, size, 1, NULL};
-			currentBlock->nextBlock = &newBlock;
-			chunk->maxSizeBetweenBlocks = chunk->maxSizeBetweenBlocks - newBlock->size - sizeof(newBlock);
-		} else if (chunk->nextChunk == NULL){
-			//CREER NOUVEAU CHUNK
-			memoryChunk *chunk = malloc(chunkSize);
-			block block = {0, size, 1, NULL};
-			chunk->head = &block;
-			chunk->maxSizeBetweenBlocks = chunkSize - block.size - sizeof(block);
+			//Si espace trouvé entre 2 block
+			if (currentBlock->nextBlock != NULL){
+				block *temp = currentBlock->nextBlock;
+				block *newBlock = {currentBlock->adress + currentBlock->size + 1, size, 1, temp};
+				currentBlock->nextBlock = newBlock;
+				//Modifier quand on aura FREE car pour l'instant maxSize = taille restante à droite
+				chunk->maxSizeBetweenBlocks = chunk->maxSizeBetweenBlocks - newBlock->size - sizeof(newBlock);
+				return &newBlock + sizeof(newBlock);
+			} else {
+				//Si aucun prochain bloc et assez d'espace pour en ajouter un à droite
+				block *newBlock = {currentBlock->adress + currentBlock->size + 1, size, 1, NULL};
+				currentBlock->nextBlock = &newBlock;
+				//Modifier quand on aura FREE car pour l'instant maxSize = taille restante à droite
+				chunk->maxSizeBetweenBlocks = chunk->maxSizeBetweenBlocks - newBlock->size - sizeof(newBlock);
+				return &newBlock + sizeof(newBlock);
+			}
 		} else {
-			//PASSER AU PROCHAIN CHUNK
-			while(chunk->nextChunk != NULL && chunk->maxSizeBetweenBlocks < size){
+			//tant que pas assez d'espace dans le chunk et prochain chunk disponible, passer au prochain chunk
+			while(chunk->nextChunk != NULL && chunk->maxSizeBetweenBlocks < size + sizeof(block)){
 				chunk = chunk->nextChunk;
+			}
+			//si prochain chunk vide et pas assez d'espace dans le dernier visité, en créer un nouveau
+			if(chunk->nextChunk == NULL && chunk->maxSizeBetweenBlocks < size + sizeof(block)){
+				memoryChunk *newChunk = malloc(chunkSize);
+				block block = {chunkSize*numberOfChunks+1, size, 1, NULL};
+				newChunk->head = &block;
+				newChunk->maxSizeBetweenBlocks = chunkSize - block.size - sizeof(block);
+				chunk->nextChunk = newChunk;
+				numberOfChunks++;
+				return &block + sizeof(block);
+			} else {
+				//Si assez d'espace pour ajouter un bloc dans le dernier chunk visité
+				block *currentBlock = chunk->head;
+				//trouver un espace entre 2 blocks ou arriver au dernier block
+				while(currentBlock->nextBlock !=NULL && currentBlock->nextBlock->adress - (currentBlock->size + currentBlock->adress) < size){
+					currentBlock = currentBlock->nextBlock;
+				}
+				//Si espace trouvé entre 2 block
+				if (currentBlock->nextBlock != NULL){
+					block *temp = currentBlock->nextBlock;
+					block *newBlock = {currentBlock->adress + currentBlock->size + 1, size, 1, temp};
+					currentBlock->nextBlock = newBlock;
+					//Modifier quand on aura FREE car pour l'instant maxSize = taille restante à droite
+					chunk->maxSizeBetweenBlocks = chunk->maxSizeBetweenBlocks - newBlock->size - sizeof(newBlock);
+					return &newBlock + sizeof(newBlock);
+				} else {
+					//Si aucun prochain bloc et assez d'espace pour en ajouter un à droite
+					block *newBlock = {currentBlock->adress + currentBlock->size + 1, size, 1, NULL};
+					currentBlock->nextBlock = &newBlock;
+					//Modifier quand on aura FREE car pour l'instant maxSize = taille restante à droite
+					chunk->maxSizeBetweenBlocks = chunk->maxSizeBetweenBlocks - newBlock->size - sizeof(newBlock);
+					return &newBlock + sizeof(newBlock);
+				}
 			}
 		}
 	}
