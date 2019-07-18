e211501525133d98696833b79be92254f15c2e8c
Raphael Guillemin <raphael.guillemin@umontreal.ca>
2019-07-18 15:50:24 -0400

    début de code

--- mymalloc.c
+++ mymalloc.c
@@ -2,20 +2,39 @@
 // Tout votre code doit être dans ce fichier
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdbool.h>
 #include "mymalloc.h"
+
+void *head = NULL;
+int remainingSize;
+
+typedef struct memoryBlock memoryBlock;
+
+struct memoryBlock{
+	size_t size;
+	void *adress;
+	int count;
+	memoryBlock *nextBlock;
+};
 
 void *mymalloc(size_t size){
   // doit retourner une plage mémoire assez large.
-  return malloc(size);
+  // première étape : définir un bloc de 4*1024 octets avec malloc
+  	if (&head==NULL){
+		head=malloc(4*1024);
+		remainingSize = 4*1024;
+	}
+	
+	remainingSize=remainingSize-size;
+	
 }
 
 int refinc(void *ptr){
   // incrémenter un compteur interne
-  return 0;
+  	return 0;
 }
 
 void myfree(void *ptr){
   // décrémente le compteur et libère la mémoire si et seulement si ce dernier est à zéro.
-  free(ptr);
+  	free(ptr);
 }
